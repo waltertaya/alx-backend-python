755f57917e30022ad5f04bcef085b2203b029ccc
waltertaya <waltermittybecker5@gmail.com>
2024-06-10 16:57:18 +0300

    Updated: Task 0 -> Requirement fail

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
     if not (isinstance(max_delay, (int, float))):
         return
     results = random.uniform(0, max_delay)
