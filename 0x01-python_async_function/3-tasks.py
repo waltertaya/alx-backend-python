d8c99d14dacf2157a0d72a5ae32c4a1a2d6e26cb
waltertaya <waltermittybecker5@gmail.com>
2024-06-10 18:16:28 +0300

    Updated: Task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,4 +1,4 @@
-#!/bin/usr/env python3
+#!/usr/bin/env python3
 ''' 3-tasks.py file
 '''
 
@@ -11,5 +11,4 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     Args: max_delay: int
     Returns: asyncio.Task
     '''
-    loop = asyncio.get_event_loop()
-    return loop.create_task(wait_random(max_delay))
+    return asyncio.create_task(wait_random(max_delay))
