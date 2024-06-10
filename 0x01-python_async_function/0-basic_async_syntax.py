bf9bfff491758d45c73a9f507c8c0cb7a916ad93
waltertaya <waltermittybecker5@gmail.com>
2024-06-10 17:01:26 +0300

    Removed: Checking if args is int or float -> unnecessary

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,10 +3,7 @@
 import asyncio
 import random
 
-
 async def wait_random(max_delay=10):
-    if not (isinstance(max_delay, (int, float))):
-        return
-    results = random.uniform(0, max_delay)
-    await asyncio.sleep(results)
-    return results
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
