f3f6682a549393aebaee4c0518d2c6ef6f1a78e1
waltertaya <waltermittybecker5@gmail.com>
2024-06-10 17:37:10 +0300

    Fixed: spawning coroutines and gather them (concurrently)

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,16 +3,16 @@
 '''
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Implements wait_n async routine
     Args: n -> int, max_delay -> int
     Body: spawn wait_random n times with the specified max_delay
-    Return: list of the all the delays(float values)
+    Return: list of all the delays (float values)
     '''
-    response_list = []
-    for i in range(n):
-        response_list.append(await wait_random(max_delay))
-    return response_list
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
