9f09109c871d3c49dabe55a2625649e3fe220df2
waltertaya <waltermittybecker5@gmail.com>
2024-07-01 18:09:28 +0300

    Fixed: Requirement Fail 🔧 2nd trial ✅✅

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -64,7 +64,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
         with patch.object(TestClass, 'a_method',
-                          return_value=lambda: 42) as mock_method:
+                          return_value=42) as mock_method:
             test_instance = TestClass()
             result1 = test_instance.a_property
             result2 = test_instance.a_property
