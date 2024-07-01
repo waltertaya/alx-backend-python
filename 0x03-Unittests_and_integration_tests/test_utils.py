ec9182de0da2fd64e866fdb2b8ef38527d60f19e
waltertaya <waltermittybecker5@gmail.com>
2024-07-01 17:06:10 +0300

    Completed: Task 2 ✅✅

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,12 +3,13 @@
 '''
 
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    ''' Testcases
+    ''' Testcases for access_nested_map
     '''
 
     @parameterized.expand([
@@ -26,6 +27,27 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    ''' Testcases for get_json
+    '''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        mock_get.assert_called_with(test_url)
+
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
