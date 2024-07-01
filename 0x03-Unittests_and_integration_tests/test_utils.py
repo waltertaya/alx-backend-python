1a1eb519752db13f5fc324e027aa04ac63c48838
waltertaya <waltermittybecker5@gmail.com>
2024-07-01 18:14:16 +0300

    Fixed: Requirement Fail  ✅🔧 ✅

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -51,11 +51,9 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    ''' Testcases for memoize
-    '''
-
+    """Tests the `memoize` function."""
     def test_memoize(self) -> None:
-        ''' Tests the memoize`s output '''
+        """Tests `memoize`'s output."""
         class TestClass:
             def a_method(self):
                 return 42
@@ -63,13 +61,14 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-        with patch.object(TestClass, 'a_method',
-                          return_value=42) as mock_method:
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as mock_method:
             test_instance = TestClass()
-            result1 = test_instance.a_property
-            result2 = test_instance.a_property
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
+            self.assertEqual(test_instance.a_property(), 42)
+            self.assertEqual(test_instance.a_property(), 42)
             mock_method.assert_called_once()
 
 
