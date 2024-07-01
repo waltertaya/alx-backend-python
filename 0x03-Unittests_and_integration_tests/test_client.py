cdb7201e0e68eeed75d8ee6a189547c19b677daa
waltertaya <waltermittybecker5@gmail.com>
2024-07-01 19:42:15 +0300

    Added: Task 4 ✅

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
-''' Unittest
+''' Unittest for the client.py
 '''
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    ''' TestCases for GithubOrgClient class
+    '''
+
+    @parameterized.expand([
+        ('google',),
+        ('abc',),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        test_instance = GithubOrgClient(org_name)
+        test_instance.org()
+        mock_get_json.called_with_once(test_instance.ORG_URL.format(org=org_name))
+
+
+if __name__ == '__main__':
+    unittest.main()
